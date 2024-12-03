ea819aa29f1f41e469b1af32ac3070c6cfa5ffd7
Tarabanchuk Ekaterina <summertime.99Fj@gmail.com>
2024-12-03 23:22:49 +0400

    passing grade func

---
+++ JS Algorithms and Data Structures/passing grade.js
@@ -0,0 +1,37 @@
+// DESCRIPTION:
+
+// The teacher is really happy with the program you have created so far.
+// But now they want to have an easy way to check if a student has a passing grade.
+// A passing grade is anything that is not an "F".
+// Complete the function hasPassingGrade that takes a student score as a parameter.
+// Your function should return true if the student has a passing grade and false if they do not.
+//     Use the getGrade function to get the student's grade. Then check if the grade is passing or not.
+
+// SOLUTION:
+function getGrade(score) {
+  if (score === 100) {
+    return "A++";
+  } else if (score >= 90) {
+    return "A";
+  } else if (score >= 80) {
+    return "B";
+  } else if (score >= 70) {
+    return "C";
+  } else if (score >= 60) {
+    return "D";
+  } else {
+    return "F";
+  }
+}
+
+function hasPassingGrade(score) {
+  if (getGrade(score) === "F") {
+    return false;
+  }
+  return true;
+}
+
+
+console.log(hasPassingGrade(100));
+console.log(hasPassingGrade(53));
+console.log(hasPassingGrade(87));
